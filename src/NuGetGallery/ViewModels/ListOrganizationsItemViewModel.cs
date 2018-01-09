36be7df55ea02c25cec1b6555d9cd6cc5d13e0dc
Scott Bommarito <scottbom@microsoft.com>
2018-01-09 15:56:46 -0800

    Add manage organizations page

---
+++ src/NuGetGallery/ViewModels/ListOrganizationsItemViewModel.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NuGetGallery
+{
+    public class ListOrganizationsItemViewModel
+    {
+        public string Username { get; }
+        public string EmailAddress { get; }
+        public int MemberCount { get; }
+        public int PackagesCount { get; }
+
+        public ListOrganizationsItemViewModel(Organization organization, IPackageService packageService)
+        {
+            Username = organization.Username;
+            EmailAddress = organization.EmailAddress;
+            MemberCount = organization.Members.Count();
+            PackagesCount = packageService.FindPackageRegistrationsByOwner(organization).Count();
+        }
+
+        public ListOrganizationsItemViewModel(Organization organization, int packagesCount)
+        {
+            Username = organization.Username;
+            EmailAddress = organization.EmailAddress;
+            MemberCount = organization.Members.Count();
+            PackagesCount = packagesCount;
+        }
+    }
+}
---
+++ src/NuGetGallery/ViewModels/ListOrganizationsViewModel.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NuGetGallery
+{
+    public class ListOrganizationsViewModel
+    {
+        public IEnumerable<ListOrganizationsItemViewModel> Organizations { get; }
+
+        public ListOrganizationsViewModel(User currentUser, IPackageService packageService)
+        {
+            // Organizations = currentUser.Organizations.Select(m => new ListOrganizationsItemViewModel(m.Organization, packageService));
+            Organizations = new[]
+            {
+                new ListOrganizationsItemViewModel(new Organization { Username = "howdy", EmailAddress = "asdf@asdf.com", Members = new Membership[] { null, null } }, 3),
+                new ListOrganizationsItemViewModel(new Organization { Username = "fff", EmailAddress = "ff2@gffff.com", Members = new Membership[] { null, null, null } }, 2)
+            };
+        }
+    }
+}
